e1a93b70f1ba4085fd77fc9399fb3766f4d5cca3
Whale Steward <onpeci@gmail.com>
2017-05-18 00:07:07 +0700

    update image adapter

--- app/src/main/java/com/steward/whale/popularmovies/ImageAdapter.java
+++ app/src/main/java/com/steward/whale/popularmovies/ImageAdapter.java
@@ -48,9 +48,7 @@ public class ImageAdapter extends BaseAdapter {
 
         if (convertView == null){
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-            imageView.setScaleType(ImageView.ScaleType.CENTER);
-            imageView.setPadding(8,8,8,8);
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
         } else {
             imageView = (ImageView) convertView;
         }
